96d92423162408befb690fdb382e93db70205561
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-01-17 10:35:57 -0500

    add router

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const morgan = require('morgan');
 const nunjucks = require('nunjucks')
+const routes = require('./routes/');
+
+app.use('/', routes);
 
 app.listen(3000, function() {
   console.log('server listening')
 });
 
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
+// app.get('/', function (req, res) {
+//   // res.send('Hello World!')
+//   const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// res.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
 
 // doesn't log GET /
 app.use('/', function (req, res, next) {
@@ -19,14 +22,16 @@ app.use('/', function (req, res, next) {
   next();
 });
 
-app.use('/special/', function (req, res, next) {
-  res.send('you reached special area');
-  next();
-});
+app.use(express.static('public'));
 
-app.use('/news', function (req, res, next) {
-  res.send('visit news')
-});
+// app.use('/special/', function (req, res, next) {
+//   res.send('you reached special area');
+//   next();
+// });
+
+// app.use('/news', function (req, res, next) {
+//   res.send('visit news')
+// });
 
 // var locals = {
 //     title: 'An Example',
